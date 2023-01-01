067202a8500da81772881de9138a7f59ad99a725
jcserv <jarrod.servilla@gmail.com>
2022-12-31 21:51:47 -0500

    chore: initialize slack app

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ez-pr-bot
+# :robot: ez-pr-bot
 A Slack bot that accelerates the PR review process for your team
+
+## Why
+
+Getting your code changes reviewed can be more laborious than it should be.
+
+EZ PR Bot solves this by encapsulating the entire code PR review workflow into a simple shortcut, and makes them actionable.
+
+- Providing a quick and easy shortcut to put up your PRs for review
+
+## Future Extensions
+
+See the Github projects to keep view our planned/upcoming work.
+
+### References
+
+https://github.com/glamboyosa/PRBot
+
+https://www.upsilonit.com/blog/create-a-slack-bot-with-typescript-in-3-steps#step-1-initial-slack-bot-setup
+
+https://www.freecodecamp.org/news/how-to-build-a-basic-slackbot-a-beginners-guide-6b40507db5c5/
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,35 @@
-console.log('Hello World!!!');
+import { App } from "@slack/bolt";
+require('dotenv').config()
+
+
+const SLACK_APP_TOKEN = process.env.SLACK_APP_TOKEN;
+const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
+
+const app = new App({
+  appToken: SLACK_APP_TOKEN,
+  token: SLACK_BOT_TOKEN,
+  socketMode: true,
+});
+
+app.command("/admins", async ({ ack, client }) => {
+  const { members } = await client.users.list();
+  const admins = members!.filter((member) => member.is_admin);
+
+  await ack({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `${admins.map((admin) => `@${admin.name}`).join(", ")}`
+        }
+      },
+    ],
+    response_type: "ephemeral", // change to "in_channel" to make it visible to others
+  });
+});
+
+app.start().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
