d1f8244a79ebf3fc76d8db109d9884ded78e3b93
jcserv <jarrod.servilla@gmail.com>
2023-03-13 02:03:52 -0400

    chore: move app-config to separate package

--- services/slack/functions/eventListener/src/appConfig.ts
+++ services/slack/functions/eventListener/src/appConfig.ts
@@ -3,49 +3,15 @@ import {
   AppOptions,
   Authorize,
   AuthorizeResult,
-  FileInstallationStore,
-  InstallationStore,
   LogLevel,
   Receiver,
 } from "@slack/bolt";
 import { StringIndexed } from "@slack/bolt/dist/types/helpers";
 import dotenv from "dotenv";
 
+import { BaseConfig } from "../../../../../libs/slackAppConfig";
+
 dotenv.config();
-
-export const scopes = [
-  "app_mentions:read",
-  "channels:join",
-  "channels:history",
-  "chat:write",
-  "chat:write.public",
-  "commands",
-  "emoji:read",
-  "im:write",
-  "reactions:read",
-  "reactions:write",
-  "users:read",
-  "usergroups:read",
-  "workflow.steps:execute",
-];
-
-class BaseConfig {
-  signingSecret: string;
-  clientId: string;
-  clientSecret: string;
-  stateSecret: string;
-  scopes: string[];
-  installationStore: InstallationStore;
-
-  constructor() {
-    this.signingSecret = process.env.SLACK_SIGNING_SECRET || "";
-    this.clientId = process.env.SLACK_CLIENT_ID || "";
-    this.clientSecret = process.env.SLACK_CLIENT_SECRET || "";
-    this.stateSecret = process.env.STATE_SECRET || "";
-    this.scopes = scopes;
-    this.installationStore = new FileInstallationStore();
-  }
-}
 
 class BaseAppConfig extends BaseConfig implements AppOptions {
   receiver: Receiver;
