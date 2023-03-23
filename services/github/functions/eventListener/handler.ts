98f86a39986019da0eec1ba096c40fa1b96b54f6
jcserv <jarrod.servilla@gmail.com>
2023-03-23 09:26:42 -0400

    feat: setup serverless deploy

--- services/github/functions/eventListener/handler.ts
+++ services/github/functions/eventListener/handler.ts
@@ -2,12 +2,9 @@ import {
   createLambdaFunction,
   createProbot,
 } from "@probot/adapter-aws-lambda-serverless";
-import { Handler } from "aws-lambda";
 
 import appFn from "./app";
 
-export const events: Handler = async () => {
-  return createLambdaFunction(appFn, {
-    probot: createProbot(),
-  });
-};
+module.exports.events = createLambdaFunction(appFn, {
+  probot: createProbot(),
+});
